56c6edf1baeff074805a879b91be452d241b7f98
bwilytsch <bwilytsch@gmail.com>
2022-08-11 18:37:52 +0100

    Further optimization

--- nvim/.config/nvim/after/plugin/null-ls.rc.lua
+++ nvim/.config/nvim/after/plugin/null-ls.rc.lua
@@ -1,4 +1,9 @@
 local status, null_ls = pcall(require, 'null-ls')
 if (not status) then return end
 
-null_ls.setup({})
+null_ls.setup({
+  sources = {
+    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.code_actions.eslint_d,
+  }
+})
