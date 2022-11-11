c0f15cf1a2e125aebfe3a4e71caba6927832ca76
bwilytsch <bwilytsch@moonpay.com>
2022-11-11 17:33:01 +0000

    Adding eslint formatting alongside prettier

--- nvim/.config/nvim/plugin/lspconfig.rc.lua
+++ nvim/.config/nvim/plugin/lspconfig.rc.lua
@@ -42,7 +42,23 @@ nvim_lsp.tsserver.setup({
 	cmd = { "typescript-language-server", "--stdio" },
 })
 
-nvim_lsp.eslint.setup({})
+nvim_lsp.eslint.setup({
+	filetypes = { "typescript", "typescriptreact", "typescript.tsx", "javascript", "javascriptreact", "javascript.jsx" },
+    flags = { debounce_text_changes = 500 },
+    on_attach = function(client, bufnr)
+      client.server_capabilities.documentFormattingProvider = true
+      if client.server_capabilities.documentFormattingProvider then
+        local au_lsp = vim.api.nvim_create_augroup("eslint_lsp", { clear = true })
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          pattern = "*",
+          callback = function()
+            vim.lsp.buf.format({ async = true })
+          end,
+          group = au_lsp,
+        })
+      end
+    end,
+})
 
 nvim_lsp.tailwindcss.setup({
 	filetypes = {
