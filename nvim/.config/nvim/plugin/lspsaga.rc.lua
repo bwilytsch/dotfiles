c7d9d5d49dd8c4a4677e9d5f3c3ca136490715e8
bwilytsch <bwilytsch@moonpay.com>
2022-11-11 16:40:25 +0000

    Updated preview to peek definition. Changed diagnostic next to only include errors.

--- nvim/.config/nvim/plugin/lspsaga.rc.lua
+++ nvim/.config/nvim/plugin/lspsaga.rc.lua
@@ -8,7 +8,7 @@ saga.init_lsp_saga({
 	-- "single" | "double" | "rounded" | "bold" | "plus"
 	border_style = "rounded",
 	saga_winblend = 0,
-	code_action_icon = "ﯦ",
+	code_action_icon = "",
 	-- finder do lsp request timeout
 	-- if your project big enough or your server very slow
 	-- you may need to increase this value
@@ -17,13 +17,15 @@ saga.init_lsp_saga({
 })
 
 local opts = { noremap = true, silent = true }
-vim.keymap.set("n", "<C-j>", "<Cmd>Lspsaga diagnostic_jump_next<cr>", opts)
+vim.keymap.set("n", "<C-j>", function()
+	require("lspsaga.diagnostic").goto_next({ severity = vim.diagnostic.severity.ERROR })
+end, opts)
 
 -- Look Up
 vim.keymap.set("n", "K", "<Cmd>Lspsaga hover_doc<cr>", opts)
 vim.keymap.set("n", "gd", "<Cmd>Lspsaga lsp_finder<cr>", opts)
 vim.keymap.set("n", "<C-k>", "<Cmd>Lspsaga signature_help<cr>", opts)
-vim.keymap.set("n", "gp", "<Cmd>Lspsaga preview_definition<cr>", opts)
+vim.keymap.set("n", "gp", "<Cmd>Lspsaga peek_definition<cr>", opts)
 vim.keymap.set("n", "gr", "<Cmd>Lspsaga rename<cr>", opts)
 
 -- Actions
