46a8b557da29ed246adcd6c90e19d812366a42a8
bwilytsch <bwilytsch@moonpay.com>
2023-01-24 10:35:33 +0000

    Including more default lsp servers like docker and rust

--- nvim/.config/nvim/after/plugin/mason.rc.lua
+++ nvim/.config/nvim/after/plugin/mason.rc.lua
@@ -3,8 +3,27 @@ if (not status) then return end
 local status_two, mason_lspconfig = pcall(require, 'mason-lspconfig')
 if (not status_two) then return end
 
-mason.setup({})
+mason.setup({
+    ui = {
+        border = 'rounded',
+        width = 0.5,
+        height = 0.6,
+    }
+})
 
 mason_lspconfig.setup ({
-  ensure_installed = { "sumneko_lua", "tailwindcss" },
+  ensure_installed = { 
+        "sumneko_lua", 
+        "tailwindcss", 
+        "astro",
+        "graphql",
+        "tsserver",
+        "taplo",
+        "solidity",
+        "rust_analyzer",
+        "marksman",
+        "html",
+        "dockerls",
+        "eslint"
+    },
 })
