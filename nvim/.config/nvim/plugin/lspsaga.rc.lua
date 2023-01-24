1d9b895e17f6c42fa71fe3189b33633ce224388c
bwilytsch <bwilytsch@moonpay.com>
2023-01-24 10:36:31 +0000

    Updating lspsaga to latest API changes

--- nvim/.config/nvim/plugin/lspsaga.rc.lua
+++ nvim/.config/nvim/plugin/lspsaga.rc.lua
@@ -1,33 +1,91 @@
 local status, saga = pcall(require, "lspsaga")
 if not status then
-	return
+    return
 end
 
-saga.init_lsp_saga({
-	server_filetype_map = {},
-	-- "single" | "double" | "rounded" | "bold" | "plus"
-	border_style = "rounded",
-	saga_winblend = 0,
-	code_action_icon = "",
-	-- finder do lsp request timeout
-	-- if your project big enough or your server very slow
-	-- you may need to increase this value
-	finder_request_timeout = 4000,
-	diagnostic_header = { "T", "T", "T", "T" },
+saga.setup({
+    ui = {
+        border = 'rounded',
+        title = false, -- enable & disable title in the float window
+        winblend = '0',
+        colors = {
+            normal_bg = '',
+            -- black = '#FFFFFF', // Change this in the future
+            -- title_bg = '', // Change this in the future
+        },
+        expand = '',
+        collaspe = '',
+        preview = '  ',
+    },
+    preview = {
+        line_above = 10,
+        line_below = 10,
+    },
+    request_timeout = 4000, -- increased for larger projects
+    symbol_in_winbar = {
+        enable = true,
+        separator = '  ',
+        hide_keyword = true,
+        show_file = true,
+        folder_level = 2,
+    },
+    outline = {
+        win_position = 'right',
+        win_with = '',
+        win_width = 30,
+        show_detail = true,
+        auto_preview = true,
+        auto_refresh = true,
+        auto_close = true,
+        custom_sort = nil,
+        keys = {
+            jump = 'o',
+            expand_collaspe = 'u',
+            quit = 'q',
+        },
+    },
 })
 
 local opts = { noremap = true, silent = true }
+
+-- Exception
 vim.keymap.set("n", "<C-j>", function()
-	require("lspsaga.diagnostic").goto_next({ severity = { min = vim.diagnostic.severity.WARN } }) --  
+    require("lspsaga.diagnostic"):goto_next({ severity = { min = vim.diagnostic.severity.WARN } })
 end, opts)
 
--- Look Up
-vim.keymap.set("n", "K", "<Cmd>Lspsaga hover_doc<cr>", opts)
-vim.keymap.set("n", "gd", "<Cmd>Lspsaga lsp_finder<cr>", opts)
-vim.keymap.set("n", "<C-k>", "<Cmd>Lspsaga signature_help<cr>", opts)
-vim.keymap.set("n", "gp", "<Cmd>Lspsaga peek_definition<cr>", opts)
-vim.keymap.set("n", "gr", "<Cmd>Lspsaga rename<cr>", opts)
+local mapper = function(mode, keys, func, desc)
+    if desc then
+        desc = 'LSP: ' .. desc
+    end
+
+    vim.keymap.set(mode, keys, func, { desc = desc, silent = true, noremap = true })
+end
+
+local nmap = function(keys, func, desc)
+    mapper('n', keys, func, desc)
+end
+
+local vmap = function(keys, func, desc)
+    mapper('v', keys, func, desc)
+end
+
+-- Goto
+nmap("gd", "<cmd>Lspsaga goto_definition<CR>", '[G]oto [D]efinition')
+nmap("gr", "<cmd>Lspsaga rename<CR>", '[R]ename File')
+
+-- Utils
+nmap("gp", "<cmd>Lspsaga peek_definition<CR>", '[D]efinition [P]eek')
+nmap("<leader>o", "<cmd>Lspsaga outline<CR>", 'Toggle [O]utline')
+nmap('K', "<cmd>Lspsaga hover_doc<CR>", 'Hover Documentation')
+
+-- Search
+nmap("<leader>sf", "<cmd>Lspsaga lsp_finder<CR>", '[S]earch [F]iles')
+
+-- Diagnostic
+nmap("<leader>sl", "<cmd>Lspsaga show_line_diagnostic<CR>", '[S]how [L]ine Diagnostic')
+nmap("<leader>sc", "<cmd>Lspsaga show_cursor_diagnostic<CR>", '[S]how [C]ursor Diagnostic')
+nmap("<leader>sb", "<cmd>Lspsaga show_buf_diagnostic<CR>", '[S]how [C]ursor Diagnostic')
 
 -- Actions
-vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", { silent = true })
-vim.keymap.set("v", "<leader>ca", "<cmd>Lspsaga range_code_action<CR>", { silent = true })
+nmap('<leader>ca', "<cmd>Lspsaga code_action<CR>")
+vmap('<leader>ca', "<cmd>Lspsaga range_code_action<CR>")
