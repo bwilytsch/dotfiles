fd845908853c08c3ff0ad7edf3133fe094135719
Bojan Wilytsch <bwilytsch@gmail.com>
2024-12-29 15:25:46 +0100

    Tweak python setup
    - Add type checking lsp
    - Custom command to toggle `pyright` type-checking

--- nvim/.config/nvim/lua/bwilytsch/plugins/lsp.lua
+++ nvim/.config/nvim/lua/bwilytsch/plugins/lsp.lua
@@ -154,13 +154,6 @@ return {
         -- clangd = {},
         gopls = {},
         rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
         eslint = {
           on_attach = function(_, bufnr)
             vim.api.nvim_create_autocmd('BufWritePre', {
@@ -171,6 +164,15 @@ return {
         },
         biome = {},
         tailwindcss = {},
+        pyright = {
+          settings = {
+            python = {
+              analysis = {
+                typeCheckingMode = 'off',
+              },
+            },
+          },
+        },
         pylsp = {
           settings = {
             pylsp = {
@@ -188,8 +190,6 @@ return {
           },
         },
 
-        --
-
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
@@ -205,6 +205,25 @@ return {
           },
         },
       }
+
+      -- Pyright type checking command, since this is not used often
+      vim.api.nvim_create_user_command('TogglePyrightTypeChecking', function()
+        local client = vim.lsp.get_clients({ name = 'pyright' })[1]
+        if client then
+          local current_setting = client.config.settings.python.analysis.typeCheckingMode
+          local new_setting = current_setting == 'off' and 'basic' or 'off'
+
+          -- Update the setting
+          client.config.settings.python.analysis.typeCheckingMode = new_setting
+
+          -- Notify the server of the change
+          client.notify('workspace/didChangeConfiguration', {
+            settings = client.config.settings,
+          })
+
+          vim.notify('Type checking: ' .. new_setting, vim.log.levels.INFO)
+        end
+      end, {})
 
       -- Ensure the servers and tools above are installed
       --  To check the current status of installed tools and/or manually install
